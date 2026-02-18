56012cb070a9846d7872827d5448a719c94d9e18
kwood <155027796+qkewq@users.noreply.github.com>
2026-02-17 16:30:46 -0800

    Create files
    Initialization

---
+++ include/epoll_data.h
@@ -0,0 +1,6 @@
+#ifndef EPOLL_DATA_H
+#define EPOLL_DATA_H
+
+
+
+#endif
---
+++ src/socksprox.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
+#include "include/config.h"
+#include "include/logger.h"
+#include "include/net.h"
+#include "include/epoll_data.h"
