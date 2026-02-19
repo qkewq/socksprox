2c95922b378efca45839b2774dbe0b67f748cd36
kwood <155027796+qkewq@users.noreply.github.com>
2026-02-19 02:33:42 -0800

    Formatting

--- src/epoll_data.c
+++ src/epoll_data.c
@@ -1,4 +1,4 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "include/epoll_data.h"
+#include "epoll_data.h"
--- src/net.c
+++ src/net.c
@@ -1,4 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
 
-#include "include/net.h"
+#include "net.h"
+
+// int bind_configs(){
+
+// }
+
+// int init_listeners(){
+
+// }
+
