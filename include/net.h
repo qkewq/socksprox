123d66fe235c3b27755e419892c321a4a7a9ae17
kwood <155027796+qkewq@users.noreply.github.com>
2026-02-20 14:44:08 -0800

    Initialize listeners

--- include/net.h
+++ include/net.h
@@ -1,6 +1,7 @@
 #ifndef NET_H
 #define NET_H
 
-
+void fd_nonblocking(int fd);
+int init_listeners(int epoll_fd, struct configs_s *configs);
 
 #endif
--- src/net.c
+++ src/net.c
@@ -5,14 +5,40 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
+#include <fcntl.h>
 
 #include "net.h"
+#include "config.h"
+#include "epoll_data.h"
 
-// int bind_configs(){
+void fd_nonblocking(int fd){
+    fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
+}
 
-// }
-
-// int init_listeners(){
-
-// }
-
+int init_listeners(int epoll_fd, struct configs_s *configs){
+    struct listen_addrs_s *current = configs->addrs; //2d linked list??
+    struct addrinfo *ai_current = NULL;
+    if(current == NULL){
+        return -1;
+    }
+    while(current != NULL){
+        ai_current = current->addr;
+        while(ai_current != NULL){
+            int sfd = socket(ai_current->ai_family, ai_current->ai_socktype, ai_current->ai_protocol);
+            if(sfd == -1){
+                return -1;
+            }
+            if(bind(sfd, ai_current->ai_addr, ai_current->ai_addrlen) == -1){
+                return -1;
+            }
+            fd_nonblocking(sfd);
+            listen(sfd, SOMAXCONN);
+            if(ep_add_listener(epoll_fd, sfd) != 0){
+                return -1
+            }
+            ai_current = ai_current->ai_next;
+        }
+        current = current->next;
+    }
+    return 0;
+}
