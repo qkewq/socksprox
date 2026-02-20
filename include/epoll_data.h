09a160f18d6377eb491fd52b80d897787a956702
kwood <155027796+qkewq@users.noreply.github.com>
2026-02-20 14:43:50 -0800

    epoll add listener

--- include/epoll_data.h
+++ include/epoll_data.h
@@ -1,6 +1,33 @@
 #ifndef EPOLL_DATA_H
 #define EPOLL_DATA_H
 
+#define TYPE_ISLISTENER 0x00
+#define TYPE_NOLISTENER 0x01
 
+#define STATE_STATELESS 0x00
+#define STATE_LISTENING 0x01
+#define STATE_CACCEPTED 0x02
+#define STATE_AUTHENTNG 0x03
+#define STATE_AUTHENTED 0x04
+#define STATE_CONNECTIN 0x05
+#define STATE_BNDLISTNG 0x06
+#define STATE_HALF      0x07
+#define STATE_FULL      0x08
+
+struct epoll_data_s{
+    uint8_t is_listener;
+    int clientfd;
+    uint8_t cfd_state;
+    int serverfd;
+    uint8_t sfd_state;
+    void *ptr;
+};
+
+struct listener_s{
+    uint8_t is_listener;
+    int listen_fd;
+};
+
+int ep_add_listener(int epoll_fd, int fd);
 
 #endif
--- src/epoll_data.c
+++ src/epoll_data.c
@@ -1,4 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "epoll_data.h"
+
+int ep_add_listener(int epoll_fd, int fd){
+    struct epoll_event ev;
+    struct listener_s *data = malloc(sizeof(struct listener_s));
+    if(data == NULL){
+        return -1;
+    }
+    memset(&ev, 0, sizeof(ev));
+    memset(data, 0, sizeof(struct listener_s));
+
+    data->is_listener = TYPE_ISLISTENER;
+    data->listen_fd = fd;
+    ev.event = EPOLLIN;
+    ev.data.ptr = data;
+
+    if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev) == -1){
+        free(data);
+        return -1;
+    }
+
+    return 0;
+}
